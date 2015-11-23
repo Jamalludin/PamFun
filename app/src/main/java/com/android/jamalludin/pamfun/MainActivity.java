cff58dd4482f354d9e3c9ad990aec75e205d69bb
Jamalludin <jamal.schneider@gmail.com>
2015-11-23 19:29:41 +0700

    make TabHost

--- app/src/main/java/com/android/jamalludin/pamfun/MainActivity.java
+++ app/src/main/java/com/android/jamalludin/pamfun/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.jamalludin.pamfun;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,13 +11,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener, TabHost.OnTabChangeListener {
 
     ViewPager viewPager;
+    TabHost tabHost;
 
 
     @Override
@@ -25,15 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         viewPager = (ViewPager) findViewById(R.id.view_pager);
-
-        List<Fragment> fragmentList = new ArrayList<Fragment>();
-        fragmentList.add(new Fragment1());
-        fragmentList.add(new Fragment2());
-        fragmentList.add(new Fragment3());
-
-        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager(), fragmentList);
-        viewPager.setAdapter(fragmentPagerAdapter);
-
+        initViewPager();
+        initTabHost();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -47,6 +43,79 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void initTabHost() {
+        tabHost = (TabHost) findViewById(R.id.tabHost);
+        tabHost.setup();
+
+        String[] tabNames = {"Tab1", "tab2", "tab3"};
+
+        for(int i = 0; i<tabNames.length; i++){
+            TabHost.TabSpec tabSpec;
+            tabSpec = tabHost.newTabSpec(tabNames[i]);
+            tabSpec.setIndicator(tabNames[i]);
+            tabSpec.setContent(new FakeContent(getApplicationContext()));
+            tabHost.addTab(tabSpec);
+        }
+
+        tabHost.setOnTabChangedListener(this);
+
+    }
+
+
+    public class FakeContent implements TabHost.TabContentFactory{
+
+        Context context;
+
+        public FakeContent(Context mcontext){
+            context = mcontext;
+
+        }
+
+        @Override
+        public View createTabContent(String tag) {
+            View fakeView = new View(context);
+            fakeView.setMinimumHeight(0);
+            fakeView.setMinimumWidth(0);
+
+            return fakeView;
+        }
+    }
+
+    private void initViewPager() {
+
+        List<Fragment> fragmentList = new ArrayList<Fragment>();
+        fragmentList.add(new Fragment1());
+        fragmentList.add(new Fragment2());
+        fragmentList.add(new Fragment3());
+
+        FragmentPagerAdapter fragmentPagerAdapter = new FragmentPagerAdapter(getSupportFragmentManager(), fragmentList);
+        viewPager.setAdapter(fragmentPagerAdapter);
+        viewPager.setOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onTabChanged(String tabId) {
+        int selectedItem = tabHost.getCurrentTab();
+        viewPager.setCurrentItem(selectedItem);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int selectedItem) {
+        tabHost.setCurrentTab(selectedItem);
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,4 +138,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
